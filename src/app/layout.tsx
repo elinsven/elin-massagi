cbc3f7292222317fdf2929a9a710340d0e0b1f40
Elin Svensell <elinsvensell@hotmail.com>
2024-01-02 17:48:32 +0100

    Added normalize css file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import "../styles/globals.css";
-import "../styles/theme.css";
+import "../styles/styles.css";
 import Header from "../components/Header/Header";
 config.autoAddCss = false;
 
